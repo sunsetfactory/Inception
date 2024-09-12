a5e658a2ed0b0f9957c7af4d89f1d8dbcd0e7440
sunsetfactory <yang1194us@gmail.com>
2024-09-12 12:48:48 +0900

    launch

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,11 +2,7 @@
 set -e
 
 # 워드프레스를 다운로드하고 압축을 풉니다
-# wget -O /tmp/wordpress.tar.gz "http://wordpress.org/latest.tar.gz"
-# tar -xvzf /tmp/wordpress.tar.gz --strip-components=1 -C /var/www/html
-
-
-wget -O /tmp/wordpress.tar.gz "http://wordpress.org/latest.tar.gz"
-tar --strip-components=1 -xvzf /tmp/wordpress.tar.gz -C /var/www/html
+wget -q -O /tmp/wordpress.tar.gz "https://wordpress.org/latest.tar.gz"
+tar -xzf /tmp/wordpress.tar.gz -C /var/www/html --strip-components=1
 
 php-fpm82 --nodaemonize
